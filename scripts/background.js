53c9357b275c8d43b11c739f94af91340f9c8479
Duc238 <92005265+Duc238@users.noreply.github.com>
2023-12-16 13:33:03 +0700

    Delete scripts directory

--- scripts/background.js
+++
@@ -1,7 +0,0 @@
-// background.js
-chrome.runtime.onConnect.addListener(function (port) {
-  console.assert(port.name == "popup");
-  port.onDisconnect.addListener(function () {
-    alert("Window is closed");
-  });
-});
--- scripts/content.js
+++
@@ -1,9 +0,0 @@
-alert("Hello Hoài Ngọc and Đức, Minh, Dung");
-function firstLoad() {
-  document.getElementsByClassName("user-account-name")[0].click();
-  document
-    .getElementsByClassName("us-links")[0]
-    .children[0].children[0].click();
-  //document.getElementsByClassName("far fa-comments")[0].click();
-}
-firstLoad();
